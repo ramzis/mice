d5addb0697492a256afc0d669a3863ebfd95928d
ramzis <ta15668@my.bristol.ac.uk>
2019-08-19 19:10:31 -0700

    Change UICanvas to use EventReceiver

--- Assets/Tadget/Mice/Scripts/Level/UICanvas.cs
+++ Assets/Tadget/Mice/Scripts/Level/UICanvas.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using static EventManager;
+using static EventReceiver;
 
 public class UICanvas : MonoBehaviour
 {
@@ -33,12 +33,12 @@ public class UICanvas : MonoBehaviour
         canvas.SetActive(active);
     }
 
-    private void UpdateCanvas(string headerText, string paraText, int starCount)
+    private void UpdateCanvas((string headerText, string paraText, int starCount) vt)
     {
-        header.text = headerText;
-        para.text = paraText;
+        header.text = vt.headerText;
+        para.text = vt.paraText;
         for (int i = 0; i < stars.Length; i++)
-            stars[i].sprite = starSprites[(int)(i < starCount ? StarSprite.FULL : StarSprite.EMPTY)];
+            stars[i].sprite = starSprites[(int)(i < vt.starCount ? StarSprite.FULL : StarSprite.EMPTY)];
     }
 
     private void UpdateTime()
@@ -49,29 +49,7 @@ public class UICanvas : MonoBehaviour
 
     private void OnEnable()
     {
-        Subscribe(Events.TOGGLE_CANVAS, ToggleCanvasEvent);
-        Subscribe(Events.UPDATE_CANVAS, UpdateCanvasEvent);
-    }
-
-    private void OnDisable()
-    {
-        Unsubscribe(Events.TOGGLE_CANVAS, ToggleCanvasEvent);
-        Unsubscribe(Events.UPDATE_CANVAS, UpdateCanvasEvent);
-    }
-
-    private void UpdateCanvasEvent(dynamic t)
-    {
-        if (t.Item1 is string && t.Item2 is string && t.Item3 is int)
-        {
-            UpdateCanvas(t.Item1, t.Item2, t.Item3);
-        }
-    }
-
-    private void ToggleCanvasEvent(dynamic t)
-    {
-        if (t is bool)
-        {
-            ToggleCanvas(t);
-        }
+        Subscribe<bool>(Events.TOGGLE_CANVAS, ToggleCanvas);
+        Subscribe<(string headerText, string paraText, int starCount)>(Events.UPDATE_CANVAS, UpdateCanvas);
     }
 }
